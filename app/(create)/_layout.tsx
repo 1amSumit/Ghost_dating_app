64eb991be997d5f00759c833c90ac0acac9d49bd
1amSumit <sj779619@gmail.com>
2025-05-18 17:39:45 +0530

    profile create routing done

--- app/(create)/_layout.tsx
+++ app/(create)/_layout.tsx
@@ -5,6 +5,23 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="username" />
+      <Stack.Screen name="email" />
+      <Stack.Screen name="password" />
+      <Stack.Screen name="otp" />
+      <Stack.Screen name="dob" />
+      <Stack.Screen name="location" />
+      <Stack.Screen name="pronouns" />
+      <Stack.Screen name="gender" />
+      <Stack.Screen name="sexuality" />
+      <Stack.Screen name="liketodate" />
+      <Stack.Screen name="intension" />
+      <Stack.Screen name="lookingfor" />
+      <Stack.Screen name="height" />
+      <Stack.Screen name="ethnicity" />
+      <Stack.Screen name="hometown" />
+      <Stack.Screen name="work" />
+      <Stack.Screen name="education" />
+      <Stack.Screen name="" />
     </Stack>
   );
 }
