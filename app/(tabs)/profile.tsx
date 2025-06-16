727807afcec1041e3d375b5da1ed6c2c21228f84
1amSumit <sj779619@gmail.com>
2025-06-16 12:30:31 +0530

    max distance input change done

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,6 @@
 import { RootState } from "@/store/store";
 import { Feather } from "@expo/vector-icons";
+import Slider from "@react-native-community/slider";
 import { useFocusEffect } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import * as Location from "expo-location";
@@ -7,6 +8,7 @@ import { useRouter } from "expo-router";
 import { useCallback, useState } from "react";
 import {
   Image,
+  ScrollView,
   StatusBar,
   Text,
   TextInput,
@@ -25,7 +27,10 @@ export default function Profile() {
     useState<boolean>(false);
   const [latitude, setLatitude] = useState<number>(0);
   const [longitude, setLongitude] = useState<number>(0);
+  const [maxDistance, setMaxDistance] = useState<number>(10);
+
   const router = useRouter();
+
   const userData = useSelector(
     (state: RootState) => state.userDataSlice.userData
   );
@@ -42,6 +47,7 @@ export default function Profile() {
       setBio(parsedUserData.user_details.bio);
       setHowyoudie(parsedUserData.user_details.howyoudie);
       setAddress(parsedUserData.user_details.location);
+      setMaxDistance(parsedUserData.preferences.max_distance);
       return () => {};
     }, [])
   );
@@ -71,7 +77,6 @@ export default function Profile() {
         const address = addressArray[0];
         const formattedAddress = `${address.name}, ${address.street}, ${address.city}, ${address.region}, ${address.postalCode}, ${address.country}`;
         setAddress(formattedAddress);
-        console.log("Current Address:", formattedAddress);
       }
     } catch (error) {
       console.error("Error getting location or address:", error);
@@ -85,85 +90,118 @@ export default function Profile() {
       end={{ x: 1, y: 1 }}
       className="flex-1 px-4 pt-2"
     >
-      <View className="">
-        <Text className="text-gray-100 text-xl mt-4 font-cinzelBold">
-          Profile
-        </Text>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 200 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="">
+          <Text className="text-gray-100 text-xl mt-4 font-cinzelBold">
+            Profile
+          </Text>
 
-        <View className="flex border-[1px] border-slate-300  flex-col items-center mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
-          <View className="relative w-20 h-20 rounded-full">
-            <Image
-              source={{ uri: parsedUserData.user_details.profile_pic }}
-              className="w-full h-full rounded-full"
-            />
-            <View className="absolute right-0 bottom-0 bg-pink-600 w-7 h-7 rounded-full flex items-center justify-center">
-              <Feather name="camera" size={16} color="white" />
+          <View className="flex border-[1px] border-slate-300  flex-col items-center mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
+            <View className="relative w-20 h-20 rounded-full">
+              <Image
+                source={{ uri: parsedUserData.user_details.profile_pic }}
+                className="w-full h-full rounded-full"
+              />
+              <View className="absolute right-0 bottom-0 bg-pink-600 w-7 h-7 rounded-full flex items-center justify-center">
+                <Feather name="camera" size={16} color="white" />
+              </View>
+            </View>
+
+            <View className="flex flex-row gap-x-1 mt-2">
+              <Text className="text-gray-100 font-cinzelBold text-sm text-center">
+                {parsedUserData.user_details.first_name}{" "}
+                {parsedUserData.user_details.last_name},
+              </Text>
+              <Text className="text-gray-100 font-cinzelBold text-sm text-center">
+                {parsedUserData.user_details.age}
+              </Text>
             </View>
           </View>
 
-          <View className="flex flex-row gap-x-1 mt-2">
-            <Text className="text-gray-100 font-cinzelBold text-sm text-center">
-              {parsedUserData.user_details.first_name}{" "}
-              {parsedUserData.user_details.last_name},
-            </Text>
-            <Text className="text-gray-100 font-cinzelBold text-sm text-center">
-              {parsedUserData.user_details.age}
-            </Text>
-          </View>
-        </View>
-
-        <View className="flex border-[1px] border-slate-300 flex-col gap-4 mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
-          <View>
-            <Text className="text-gray-100 text-sm font-cinzel">
-              Display Name
-            </Text>
-            <TextInput
-              className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
-              value={username}
-              onChangeText={setUsername}
-            />
-          </View>
-          <View>
-            <Text className="text-gray-100 text-sm font-cinzel">Bio</Text>
-            <TextInput
-              className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
-              value={bio}
-              onChangeText={setBio}
-            />
-          </View>
-          <View>
-            <Text className="text-gray-100 text-sm font-cinzel">
-              How you die!
-            </Text>
-            <TextInput
-              className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
-              value={howyoudie}
-              onChangeText={setHowyoudie}
-            />
-          </View>
-        </View>
-
-        <View className="flex border-[1px] border-slate-300 flex-col gap-4 mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
-          <View>
-            <Text className="text-gray-100 font-cinzel text-sm">Location</Text>
-            <View className="flex flex-col">
+          <View className="flex border-[1px] border-slate-300 flex-col gap-4 mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
+            <View>
+              <Text className="text-gray-100 text-sm font-cinzel">
+                Display Name
+              </Text>
               <TextInput
                 className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
-                value={address}
-                onChangeText={setAddress}
+                value={username}
+                onChangeText={setUsername}
               />
-              <View className="items-end">
-                <TouchableOpacity onPress={() => getCurrentLocation()}>
-                  <Text className="font-cinzel mt-3  text-gray-100 text-xs">
-                    Get current Location
-                  </Text>
-                </TouchableOpacity>
+            </View>
+            <View>
+              <Text className="text-gray-100 text-sm font-cinzel">Bio</Text>
+              <TextInput
+                className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
+                value={bio}
+                onChangeText={setBio}
+              />
+            </View>
+            <View>
+              <Text className="text-gray-100 text-sm font-cinzel">
+                How you die!
+              </Text>
+              <TextInput
+                className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
+                value={howyoudie}
+                onChangeText={setHowyoudie}
+              />
+            </View>
+          </View>
+
+          <View className="flex border-[1px] border-slate-300 flex-col gap-4 mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
+            <View>
+              <Text className="text-gray-100 font-cinzel text-sm">
+                Location
+              </Text>
+              <View className="flex flex-col">
+                <TextInput
+                  className="bg-gray-200/30 text-gray-100 rounded-lg mt-2 px-3 font-cinzel"
+                  value={address}
+                  onChangeText={setAddress}
+                />
+                <View className="items-end">
+                  <TouchableOpacity onPress={() => getCurrentLocation()}>
+                    <Text className="font-cinzel mt-3  text-gray-100 text-xs">
+                      Get current Location
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+
+            <View>
+              <Text className="font-cinzel text-gray-100 text-sm">
+                Maximum Distance
+              </Text>
+              <View className="flex flex-row justify-between">
+                <Slider
+                  style={{ width: 300, height: 20 }}
+                  minimumValue={10}
+                  maximumValue={100}
+                  step={1}
+                  value={maxDistance}
+                  onValueChange={setMaxDistance}
+                  minimumTrackTintColor="#FFFFFF"
+                  maximumTrackTintColor="#000000"
+                  thumbTintColor="#FF4081"
+                />
+                <Text className="text-gray-100 font-cinzelBold text-md">
+                  {maxDistance}
+                </Text>
               </View>
             </View>
           </View>
+
+          <View className="flex border-[1px] border-slate-300  flex-col  mt-8 bg-gray-200/20 rounded-xl px-4 py-6">
+            <Text className="text-sm font-cinzel text-gray-100">Gender</Text>
+          </View>
         </View>
-      </View>
-      <StatusBar backgroundColor={"#271e1e"} />
+        <StatusBar backgroundColor={"#271e1e"} />
+      </ScrollView>
     </LinearGradient>
   );
 }
