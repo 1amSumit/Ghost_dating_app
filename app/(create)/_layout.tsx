d8de43a8b35a370e4711ef4686f7c8c243000a29
1amSumit <sj779619@gmail.com>
2025-06-19 20:29:24 +0530

    fixed screen safe area view

--- app/(create)/_layout.tsx
+++ app/(create)/_layout.tsx
@@ -4,27 +4,27 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView className="flex-1 bg-white" edges={["bottom"]}>
+    <SafeAreaView className="flex-1 bg-white">
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(create)/username" />
-        <Stack.Screen name="(create)/email" />
-        <Stack.Screen name="(create)/password" />
-        <Stack.Screen name="(create)/otp" />
-        <Stack.Screen name="(create)/dob" />
-        <Stack.Screen name="(create)/address" />
-        <Stack.Screen name="(create)/pronouns" />
-        <Stack.Screen name="(create)/gender" />
-        <Stack.Screen name="(create)/sexuality" />
-        <Stack.Screen name="(create)/liketodate" />
-        <Stack.Screen name="(create)/intension" />
-        <Stack.Screen name="(create)/height" />
-        <Stack.Screen name="(create)/education" />
-        <Stack.Screen name="(create)/howyoudie" />
-        <Stack.Screen name="(create)/bio" />
-        <Stack.Screen name="(create)/uploadImages" />
+        <Stack.Screen name="username" />
+        <Stack.Screen name="email" />
+        <Stack.Screen name="password" />
+        <Stack.Screen name="otp" />
+        <Stack.Screen name="dob" />
+        <Stack.Screen name="address" />
+        <Stack.Screen name="pronouns" />
+        <Stack.Screen name="gender" />
+        <Stack.Screen name="sexuality" />
+        <Stack.Screen name="liketodate" />
+        <Stack.Screen name="intension" />
+        <Stack.Screen name="height" />
+        <Stack.Screen name="education" />
+        <Stack.Screen name="howyoudie" />
+        <Stack.Screen name="bio" />
+        <Stack.Screen name="uploadImages" />
       </Stack>
       <StatusBar
-        backgroundColor="white"
+        backgroundColor="#eee"
         barStyle="dark-content"
         translucent={false}
       />
